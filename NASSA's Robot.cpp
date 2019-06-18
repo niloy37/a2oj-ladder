b4d8d516bebe510fcce88341bd835339c8616194
Niloy Rahman <havocisgod@gmail.com>
2019-06-18 21:30:45 +0600

    Create NASSA's Robot.cpp

---
+++ NASSA's Robot.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main () {
+int t;
+cin>>t;
+string s;
+int x,y;
+while(t--){
+    cin >> s;
+    x = 0 ; y = 0; int rc = 0;
+    for(char c : s){ //modified to casual loop during submission
+
+        if(c == 'U'){
+            y++;
+        }
+        else if(c == 'R'){
+            x++;
+
+        }
+        else if(c == 'D'){
+            y--;
+        }
+        else if(c == 'L'){
+            x--;
+        }
+        else
+            rc++;
+
+}
+        cout<< x-rc <<" "<< y-rc <<" " << x+rc <<" " << y+rc<<endl;
+
+}
+
+
+
+
+}
